3f021fbe7616ee022be219bf4f022f2c35e6b52f
sonchaegeon <thscorjs1002@naver.com>
2021-03-22 14:57:19 +0900

    [UPDATE] auth sign up service

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,6 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { SignUpUserDto } from './entity/user.dto';
+import { User } from './entity/user.entity';
+import { UserRepository } from './entity/user.repository';
 
 @Injectable()
 export class AuthService {
-  constructor() {}
+  constructor(@InjectRepository(User) private userRepository: UserRepository) {}
+
+  public async signUp(dto: SignUpUserDto): Promise<void> {
+    if (await this.userRepository.findUserByEmail(dto.email)) {
+      throw new BadRequestException('Email is already registered');
+    }
+    await this.userRepository.signUp(dto);
+  }
 }
