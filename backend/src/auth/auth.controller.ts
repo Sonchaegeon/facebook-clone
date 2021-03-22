486946253c56ba34c7c70282c770692df394faf8
sonchaegeon <thscorjs1002@naver.com>
2021-03-22 14:57:29 +0900

    [UPDATE] auth sign up controller

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,6 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { SignUpUserDto } from './entity/user.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor() {}
+  constructor(private authService: AuthService) {}
+
+  @Post('register')
+  async signUp(@Body() body: SignUpUserDto) {
+    await this.authService.signUp(body);
+    return { message: 'Success' };
+  }
 }
