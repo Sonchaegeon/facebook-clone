74cdd0eba2ec51c927d5bc78147f594b4cf9c6cc
sonchaegeon <thscorjs1002@naver.com>
2021-03-23 22:43:32 +0900

    [CREATE] register make account api

---
+++ frontend/src/lib/api/Register/index.ts
@@ -0,0 +1,12 @@
+import { apiDefault } from '../client';
+import { RegisterReq } from '../payloads/Register';
+
+export const makeAccount = (data: RegisterReq) => {
+  console.log(data);
+  return apiDefault().post('/auth/register', {
+    ...data,
+    birth_year: parseInt(data.birth_year),
+    birth_month: parseInt(data.birth_month),
+    birth_day: parseInt(data.birth_day),
+  });
+};
