9f1531efb0b5cad940f6609db228df017634c958
sonchaegeon <thscorjs1002@naver.com>
2021-03-23 22:42:17 +0900

    [UPDATE] option keys, getAxiosError function

--- frontend/src/lib/utils/function/index.tsx
+++ frontend/src/lib/utils/function/index.tsx
@@ -1,12 +1,19 @@
+import { AxiosError } from 'axios';
+import { ResDefault } from '../../api/payloads';
+
 export const setYearDate = () => {
   let yearArray = [];
   yearArray.push(
-    <option value="0" selected>
+    <option value="0" key={0}>
       연도
     </option>
   );
   for (let i = 2021; i > 1920; i--) {
-    yearArray.push(<option value={i}>{i}년</option>);
+    yearArray.push(
+      <option key={i} value={i}>
+        {i}년
+      </option>
+    );
   }
   return yearArray;
 };
@@ -14,12 +21,16 @@ export const setYearDate = () => {
 export const setMonthDate = () => {
   let monthArray = [];
   monthArray.push(
-    <option value="0" selected>
+    <option value="0" key={0}>
       월
     </option>
   );
   for (let i = 1; i <= 12; i++) {
-    monthArray.push(<option value={i}>{i}월</option>);
+    monthArray.push(
+      <option key={i} value={i}>
+        {i}월
+      </option>
+    );
   }
   return monthArray;
 };
@@ -27,12 +38,22 @@ export const setMonthDate = () => {
 export const setDayDate = () => {
   let dayArray = [];
   dayArray.push(
-    <option value="0" selected>
+    <option value="0" key={0}>
       일
     </option>
   );
   for (let i = 1; i <= 31; i++) {
-    dayArray.push(<option value={i}>{i}일</option>);
+    dayArray.push(
+      <option key={i} value={i}>
+        {i}일
+      </option>
+    );
   }
   return dayArray;
+};
+
+export const getAxiosError = (err: AxiosError<ResDefault>) => {
+  console.log(err.response.data);
+  const { status, code } = err.response.data;
+  return { status, code };
 };
