b1b04dc43aee1deab99488bd3a6e70d71624b33b
sonchaegeon <thscorjs1002@naver.com>
2021-03-29 08:59:09 +0900

    [CREATE] icon upload

--- frontend/src/assets/Main/index.ts
+++ frontend/src/assets/Main/index.ts
@@ -1,1 +1,5 @@
 export { default as LogoImage } from './Logo.svg';
+export { default as HeaderIconImage } from './HeaderIcon.png';
+export { default as HomeIconImage } from './HomeIcon.png';
+export { default as FriendsIconImage } from './FriendsIcon.png';
+export { default as GroupIconImage } from './GroupIcon.png';
