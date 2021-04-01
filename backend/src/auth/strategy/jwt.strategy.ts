eed0ed6219a6e085f4649f1ec003df7449cfe707
sonchaegeon <thscorjs1002@naver.com>
2021-04-02 00:56:59 +0900

    [CREATE] jwt strategy

---
+++ backend/src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { TokenPayload } from '../interface/payload.interface';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: TokenPayload): Promise<TokenPayload> {
+    return { id: payload.id };
+  }
+}
