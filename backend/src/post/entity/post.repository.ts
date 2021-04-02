6ac9d406de0fe74901fec7d80f91b2e4323e4c4c
sonchaegeon <thscorjs1002@naver.com>
2021-04-02 10:57:49 +0900

    [UPDATE] create post repository

--- backend/src/post/entity/post.repository.ts
+++ backend/src/post/entity/post.repository.ts
@@ -1,9 +1,13 @@
+import { User } from 'src/auth/entity/user.entity';
 import { EntityRepository, Repository } from 'typeorm';
 import { GetPostsArgs } from '../dto/args/get-posts.args';
+import { CreatePostInput } from '../dto/input/create-post.input';
 import { Post } from './post.entity';
 
 @EntityRepository(Post)
 export class PostRepository extends Repository<Post> {
+  private newPost: Post;
+
   public async getPosts(getPostsArgs: GetPostsArgs): Promise<Post[]> {
     return this.createQueryBuilder('post')
       .innerJoin('post.user', 'user')
@@ -11,5 +15,13 @@ export class PostRepository extends Repository<Post> {
       .addSelect('post.contents', 'contents')
       .where('user.id = :user_id', { user_id: getPostsArgs.userId })
       .getRawMany();
+  }
+
+  public async createPost(
+    createPostData: CreatePostInput,
+    user: User,
+  ): Promise<Post> {
+    this.newPost = this.create({ ...createPostData, user });
+    return await this.save(this.newPost);
   }
 }
