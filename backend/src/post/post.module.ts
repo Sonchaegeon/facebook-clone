2ebec9ca80cff3ab7f6c1e7465028b8b598fcd96
sonchaegeon <thscorjs1002@naver.com>
2021-04-03 20:48:14 +0900

    [UPDATE] post module import post user view repository

--- backend/src/post/post.module.ts
+++ backend/src/post/post.module.ts
@@ -1,12 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from 'src/auth/entity/user.repository';
+import { PostUserViewRepository } from './entity/post-view.repository';
 import { PostRepository } from './entity/post.repository';
 import { PostResolver } from './post.resolver';
 import { PostService } from './post.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PostRepository, UserRepository])],
+  imports: [
+    TypeOrmModule.forFeature([
+      PostRepository,
+      UserRepository,
+      PostUserViewRepository,
+    ]),
+  ],
   providers: [PostResolver, PostService],
 })
 export class PostModule {}
