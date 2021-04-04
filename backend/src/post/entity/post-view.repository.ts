53194999a62025f2238e0ddeea90cfc0e4ecc60d
sonchaegeon <thscorjs1002@naver.com>
2021-04-04 23:29:15 +0900

    [UPDATE] post view repository exist user post function

--- backend/src/post/entity/post-view.repository.ts
+++ backend/src/post/entity/post-view.repository.ts
@@ -6,11 +6,23 @@ import { Post } from './post.entity';
 @EntityRepository(PostUserView)
 export class PostUserViewRepository extends Repository<PostUserView> {
   public async getPosts(getPostsArgs: GetPostsArgs): Promise<Post[]> {
-    return this.createQueryBuilder('view')
+    return await this.createQueryBuilder('view')
       .select('view.contents', 'contents')
       .addSelect('view.post_created_at', 'created_at')
       .addSelect('view.post_id', 'id')
       .where('view.user_id = :user_id', { user_id: getPostsArgs.userId })
       .getRawMany();
+  }
+
+  public async existUserPost(postId: number, userId: number): Promise<boolean> {
+    const postRecord = await this.createQueryBuilder('view')
+      .select('view.contents', 'contents')
+      .where('view.user_id = :user_id AND view.post_id = :post_id', {
+        user_id: userId,
+        post_id: postId,
+      })
+      .getRawOne();
+
+    return !!postRecord;
   }
 }
