eddab5ff414e621e335af47bacea63d689bedc04
sonchaegeon <thscorjs1002@naver.com>
2021-04-04 23:27:54 +0900

    [UPDATE] graphql exception error

--- backend/src/auth/strategy/local.strategy.ts
+++ backend/src/auth/strategy/local.strategy.ts
@@ -1,6 +1,7 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
+import { UnauthorizedError } from 'src/common/http-exception.index';
 import { AuthService } from '../auth.service';
 
 @Injectable()
@@ -12,7 +13,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   async validate(email: string, password: string): Promise<any> {
     const user = await this.authService.validateUser(email, password);
     if (!user) {
-      throw new UnauthorizedException();
+      throw UnauthorizedError;
     }
     return user;
   }
