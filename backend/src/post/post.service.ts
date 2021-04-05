7e8707e6ff60c3fe36668ed0caedb53036fbe576
sonchaegeon <thscorjs1002@naver.com>
2021-04-05 13:04:35 +0900

    [UPDATE] post service delete post

--- backend/src/post/post.service.ts
+++ backend/src/post/post.service.ts
@@ -4,7 +4,7 @@ import { User } from 'src/auth/entity/user.entity';
 import { UserRepository } from 'src/auth/entity/user.repository';
 import { ForbiddenError, NotFoundError } from 'src/common/http-exception.index';
 import { GetPostsArgs } from './dto/args/get-posts.args';
-import { CreatePostInput, UpdatePostInput } from './dto/input';
+import { CreatePostInput, UpdatePostInput, DeletePostInput } from './dto/input';
 import { PostUserView } from './entity/post-view.entity';
 import { PostUserViewRepository } from './entity/post-view.repository';
 import { Post } from './entity/post.entity';
@@ -53,5 +53,25 @@ export class PostService {
 
     await this.postRepository.updatePost(updatePostData);
     return await this.postRepository.findOne(updatePostData.id);
+  }
+
+  public async deletePost(
+    deletePostData: DeletePostInput,
+    userId: number,
+  ): Promise<Post> {
+    const user = await this.userRepository.findOne(userId);
+    const isExist = await this.postUserViewRepository.existUserPost(
+      deletePostData.id,
+      user.id,
+    );
+    if (!user) {
+      throw NotFoundError;
+    }
+    if (!isExist) {
+      throw ForbiddenError;
+    }
+
+    await this.postRepository.deletePost(deletePostData);
+    return isExist;
   }
 }
